6d910d9423affd31b7188d426530ad034fc928d8
Magno Valdetaro <mvaldetaro@gmail.com>
2021-12-13 10:34:01 -0300

    Remove HOCs
    Migrados para investira.components

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-export { default } from './src/index.js';
+// Utils
+export { default as renders } from './src/utils/renders';
+export { default as displays } from './src/utils/displays';
+export { default as currency } from './src/utils/currency';
+export { default as charts } from './src/utils/charts';
+export { default as browsers } from './src/utils/browsers';
+export { default as localStorages } from './src/utils/localStorages';
+export { default as lists } from './src/utils/lists';
+export { default as vibrate } from './src/utils/vibrate';
+export { default as anime } from './src/utils/anime';
+
+// Middleware
+export { default as bindStateToLocalStorage } from './src/middleware/bindStateToLocalStorage';
+
+// Store
+export { default as createStore } from './src/redux/createStore';
+export { default as persistStore } from './src/redux/persistStore';
+export { default as combineReducers } from './src/redux/combineReducers';
+export { default as combineReducersLegacy } from './src/redux/combineReducersLegacy';
+export { default as persisterReducers } from './src/redux/persisterReducers';
+export { default as persistGateComponent } from './src/redux/persistGateComponent';
+
+// Storage
+export { default as createStorage } from './src/storage/createStorage';
+export { default as listenStorage } from './src/storage/listenStorage';
+export { default as pouchStorage } from './src/storage/pouchStorage';
+export { default as idbStorage } from './src/storage/idbStorage';
+
+// HOOKs
+export { default as useLongPress } from './src/hooks/useLongPress';
+
+// HOCs
+// export { default as withDialog } from './src/hoc/withDialog';
+// export { default as infiniteScroll } from './src/hoc/infiniteScroll';
+// export { default as withMediaQueries } from './src/hoc/withMediaQueries';
+// export { default as withRedux } from './src/hoc/withRedux';
