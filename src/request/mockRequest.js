18b7ba648c59f21f76585e8cfbd66f1a8afddb62
Magno Valdetaro <mvaldetaro@gmail.com>
2023-09-14 17:43:45 -0300

    Define status 200 como default

--- src/request/mockRequest.js
+++ src/request/mockRequest.js
@@ -1,4 +1,4 @@
-function mockRequest(pData, pOptions, pWait = 200) {
+function mockRequest(pData, pOptions = { status: 200 }, pWait = 200) {
     const xResponse = new Response(JSON.stringify(pData), pOptions);
 
     return new Promise((resolve, reject) => {
